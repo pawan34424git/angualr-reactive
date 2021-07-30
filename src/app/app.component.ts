f9fab773fd407815868cb76fd6d678f15ff142b0
Pawan Kumar <pawankumar34424@gmail.com>
2021-07-30 23:13:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'reactive';
+  customerName = 'reactive';
+  dataList: any[] = [];
+  constructor(private http: HttpClient, private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.appService.getServerSentEvent('customer/stream').subscribe((res) => {
+      const {data} = res;
+      this.dataList.push(data);
+
+    });
+  }
+  submit() {
+    const data={
+      customerName: this.customerName
+    }
+    this.http.post('customer',data).toPromise();
+    this.customerName = '';
+
+  }
 }
